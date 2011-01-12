5381767a3ff52f352c038b12f93f621eb09a3c44
Henri Bergius <henri.bergius@iki.fi>
2011-01-12 12:05:02 +0200

    Switch from category tree to proper nodes

--- controllers/latest.php
+++ controllers/latest.php
@@ -6,44 +6,14 @@ class fi_openkeidas_articles_controllers_latest
         $this->request = $request;
     }
 
-    private function check_categories(midgard_query_builder $qb, array $args)
-    {
-        $categories = midgardmvc_core::get_instance()->configuration->categories;
-
-        if (!isset($args['category']))
-        {
-            return;
-        }
-
-        // Check that the user-provided category is valid
-        if (!isset($categories[$args['category']]))
-        {
-            throw new midgardmvc_exception_notfound("Category {$args['category']} not found");
-        }
-
-        if (isset($args['subcategory']))
-        {
-            if (!isset($categories[$args['category']]['categories'][$args['subcategory']]))
-            {
-                throw new midgardmvc_exception_notfound("Category {$args['category']}/{$args['subcategory']} not found");
-            }
-
-            $qb->add_constraint('category', '=', "{$args['category']}/{$args['subcategory']}");
-            $this->data['title'] = $categories[$args['category']]['categories'][$args['subcategory']]['title'];
-
-        }
-        else
-        {
-            $qb->add_constraint('category', 'LIKE', "{$args['category']}/%");
-            $this->data['title'] = $categories[$args['category']]['title'];
-        }
-    }
-
     public function get_items(array $args)
     {
         $this->data['title'] = 'Uusimmat artikkelit';
         $qb = new midgard_query_builder('fi_openkeidas_articles_article');
-        $this->check_categories($qb, $args);
+        
+        $node = $this->request->get_node()->get_object();
+        $qb->add_constraint('node', 'INTREE', $node->id);
+
         $qb->add_order('metadata.created', 'DESC');
         $qb->set_limit(midgardmvc_core::get_instance()->configuration->index_items);
         $items = $qb->execute();
@@ -55,7 +25,5 @@ class fi_openkeidas_articles_controllers_latest
             $item->url = midgardmvc_core::get_instance()->dispatcher->generate_url('item_read', array('item' => $item->guid), $this->request);
             $this->data['items'][] = $item;
         }
-
-
     }
 }
--- injector.php
+++ injector.php
@@ -13,13 +13,37 @@ class fi_openkeidas_articles_injector
 {
     public function inject_process(midgardmvc_core_request $request)
     {
-        // Subscribe to content changed signals from Midgard
-        //midgard_object_class::connect_default('fi_openkeidas_articles_article', 'action-create', array('fi_openkeidas_articles_injector', 'check'), array($request));
+        static $connected = false;
+
+        if (!$connected)
+        {
+            // Subscribe to content changed signals from Midgard
+            midgard_object_class::connect_default('fi_openkeidas_articles_article', 'action-create', array('fi_openkeidas_articles_injector', 'check_node'), array($request));
+            $connected = true;
+        }
     }
 
-    public static function check($article, $params)
+    public static function check_node(fi_openkeidas_articles_article $article, $params)
     {
-        return;
+        if ($article->node)
+        {
+            return;
+        }
+        
+        $request = midgardmvc_core::get_instance()->context->get_request();
+        $node = $request->get_node();
+        if (!$node)
+        {
+            return;
+        }
+
+        $node_object = $node->get_object();
+        if (!$node_object instanceof midgardmvc_core_node)
+        {
+            return;
+        }
+
+        $article->node = $node_object->id;
     }
 }
 ?>
